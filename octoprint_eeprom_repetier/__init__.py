5f35cc14af8f4bcf189c49462c87b014eb7b5244
robborsari <robborsari@gmail.com>
2021-02-18 00:35:17 -0500

    Update __init__.py
    Added python 3 tag

--- octoprint_eeprom_repetier/__init__.py
+++ octoprint_eeprom_repetier/__init__.py
@@ -41,6 +41,7 @@ class Eeprom_repetierPlugin(octoprint.plugin.AssetPlugin,
         )
 
 __plugin_name__ = "EEPROM Repetier Editor Plugin"
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
     global __plugin_implementation__
